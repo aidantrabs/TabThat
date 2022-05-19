bb874b13a88ea527d9c2fd10e4aaf0fb27d10a67
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-05-19 07:46:12 -0400

    Create MongoDB connection

--- main.go
+++ main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"example/bookmark-api/controllers"
+	"example/bookmark-api/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ctx 				context.Context
+	err 				error
+
+	bms 				services.BookmarkService
+	bmc 				controllers.BookmarkController
+
+	client 			*mongo.Client
+	bookmarkcoll   	*mongo.Collection
+	server 			*gin.Engine
+)
+
+func init() {
+	ctx = context.TODO()
+
+	// Connect to MongoDB
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	
+	// Check the connection
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected successfully")
+
+	// Get a handle for your collection
+	bookmarkcoll = client.Database("bookmarks").Collection("entries")
+	bms = services.NewBookmarkService(bookmarkcoll, ctx)
+	bmc = controllers.New(bms)
+
+	server = gin.Default()
+}
+
+func main() {
+	defer client.Disconnect(ctx)
+
+	defaultpath := server.Group("/v1")
+	bmc.RegisterRoutes(defaultpath)
+
+	log.Fatal(server.Run(":8080"))
 }
