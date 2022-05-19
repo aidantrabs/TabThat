49d0a0a5a97e3496767dbe4f75df5d971b62abe4
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-05-19 07:46:25 -0400

    Update model

--- models/bm.go
+++ models/bm.go
@@ -1,7 +1,7 @@
 package models
 
 type Bookmark struct {
-	ID 			int 		`json:"id" bson:"_id"` 
+	ID 			int 		`json:"id" bson:"id"` 
 	Name 		string 	`json:"name" bson:"name"`
 	URL 			string 	`json:"url" bson:"url"`
 	Description 	string 	`json:"description" bson:"description"`
